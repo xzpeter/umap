30d00b5b25e7bed8b65b785b0cda76aaeec52a4d
IP <56741918+IBPeng@users.noreply.github.com>
2020-03-06 12:09:08 -0800

    client lookup server address from string

--- src/umap/store/rpc_client.cpp
+++ src/umap/store/rpc_client.cpp
@@ -8,7 +8,8 @@
 #include "rpc_util.hpp"
 #include "rpc_client.hpp"
 
-static char* get_server_address(){
+/* Read the server address published in the file */
+static char* get_server_address_string(){
 
   char* addr = NULL;
 
@@ -34,53 +35,61 @@ static char* get_server_address(){
 
 static margo_instance_id setup_margo_client(){
 
-  char* svr_addr_string = get_server_address();
-
-  //get the protocol used by the server
-  char* protocol = strdup(svr_addr_string);
+  /* get the protocol used by the server */
+  char* server_address_string = get_server_address_string();
+  char* protocol = strdup(server_address_string);
   char* del = strchr(protocol, ';');
   if (del) *del = '\0';
-  UMAP_LOG(Info, "svr_addr:"<<svr_addr_string<<" proto: "<<protocol);
+  UMAP_LOG(Info, "server :"<<server_address_string<<" protocol: "<<protocol);
   
   /* Init Margo using server's protocol */
   margo_instance_id mid;
   int use_progress_thread = 1;//flag to use a dedicated thread for running Mercury's progress loop. 
   int rpc_thread_count = 1; //number of threads for running rpc calls
   mid = margo_init(protocol, MARGO_SERVER_MODE, use_progress_thread, rpc_thread_count);
+  free(protocol);
   if (mid == MARGO_INSTANCE_NULL) {
-    UMAP_ERROR("margo_init protocol "<<protocol<<" failed");
+    free(server_address_string);
+    UMAP_ERROR("margo_init protocol failed");
     return mid;
   }
   UMAP_LOG(Info, "margo_init done");
 
-  /* TODO: want to keep this enabled all the time */
-  /* what's this do? */
-  margo_diag_start(mid);
   
-  /* Find the address of this client */
-  hg_addr_t addr;
-  hg_return_t ret = margo_addr_self(mid, &addr);
-  if (ret != HG_SUCCESS) {
-    UMAP_ERROR("margo_addr_self failed");
+  /* TODO: want to keep this enabled all the time */
+  margo_diag_start(mid);
+
+  
+  /* lookup server address from string */
+  hg_addr_t server_address = HG_ADDR_NULL;
+  margo_addr_lookup(mid, server_address_string, &server_address);
+  free(server_address_string);
+  if (server_address == HG_ADDR_NULL) {
     margo_finalize(mid);
-    return MARGO_INSTANCE_NULL;
+    UMAP_ERROR("Failed to lookup margo server address from string: "<<server_address_string);
+  }
+  
+  /* Find the address of this client process */
+  hg_addr_t client_address;
+  hg_return_t ret = margo_addr_self(mid, &client_address);
+  if (ret != HG_SUCCESS) {
+    margo_addr_free(mid, server_address);
+    margo_finalize(mid);
+    UMAP_ERROR("failed to lookup margo_addr_self on client");
   }
 
-  /* Convert the server address to string*/
-  char addr_string[128];
-  hg_size_t addr_string_len = sizeof(addr_string);
-  ret = margo_addr_to_string(mid, addr_string, &addr_string_len, addr);
+  /* Convert the address to string*/
+  char client_address_string[128];
+  hg_size_t len = sizeof(client_address_string);
+  ret = margo_addr_to_string(mid, client_address_string, &len, client_address);
   if (ret != HG_SUCCESS) {
-    UMAP_ERROR("margo_addr_to_string failed");
-    margo_addr_free(mid, addr);
+    margo_addr_free(mid, server_address);
+    margo_addr_free(mid, client_address);
     margo_finalize(mid);
-    return MARGO_INSTANCE_NULL;
+    UMAP_ERROR("failed to convert client address to string");
   }
-  UMAP_LOG(Info, "Margo client: " << addr_string);
-  margo_addr_free(mid, addr);
+  UMAP_LOG(Info, "Margo client adress: " << client_address_string);
 
-  free(svr_addr_string);
-  free(protocol);
   return mid;
 }
 
