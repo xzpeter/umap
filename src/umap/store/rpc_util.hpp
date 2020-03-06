abb4ae802b7ab9d2ad57b2965bc09532a8ec8d0e
IP <56741918+IBPeng@users.noreply.github.com>
2020-03-06 14:35:27 -0800

    fix umap_server_read_rpc

--- src/umap/store/rpc_util.hpp
+++ src/umap/store/rpc_util.hpp
@@ -1,9 +1,13 @@
 #ifndef _RPC_UTIL_H
 #define _RPC_UTIL_H
 
+#include "mercury_macros.h"
 
 #define LOCAL_RPC_ADDR_FILE "serverfile"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 /* UMap RPC input structure */
 MERCURY_GEN_PROC(umap_read_rpc_in_t,
                  ((hg_size_t)(size))\
@@ -12,8 +16,10 @@ MERCURY_GEN_PROC(umap_read_rpc_in_t,
 
 /* UMap RPC output structure */
 MERCURY_GEN_PROC(umap_read_rpc_out_t,
-		 ((hg_size_t)(size))\
 		 ((int32_t)(ret)))
 
-
+#ifdef __cplusplus
+} // extern "C"
+#endif
+  
 #endif
